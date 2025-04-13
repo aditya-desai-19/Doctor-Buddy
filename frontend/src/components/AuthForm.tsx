e6cf81f25af336f3eeb3edba7aae7254cc23d67e
Aditya <officialdesaiaditya@gmail.com>
2025-04-13 13:33:54 +0530

    Add navigation to auth form and improved ux by implementing change of focus throught arrow up and down keys

--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -14,32 +14,44 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useTranslations } from "next-intl"
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { FullPageSpinner } from "./LoadingSpinner"
 import { CreateDoctorRequest, LoginDoctorRequest } from "../../generated"
 import { handleLogin, handleSignUp } from "@/api/server"
-import { toast } from "sonner"
 import { toastError, toastSuccess } from "./Toast"
+import { useRouter } from "next/navigation"
 
 type Props = {
   formSchema: any //todo
   isSignUp: boolean
 }
 
-export default function SignUp({ formSchema, isSignUp }: Props) {
+type InputType = {
+  name: string
+  label: string
+  type: string
+  placeholder: string
+}
+
+const ARROW_UP = "ArrowUp"
+const ARROW_DOWN = "ArrowDown"
+
+export default function AuthForm({ formSchema, isSignUp }: Props) {
   const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [inputs, setInputs] = useState<InputType[]>([])
+  const [currentFocussedInputIdx, setCurrentFocussedInputIdx] = useState<number>(0)
+
   const t = useTranslations()
+  const router = useRouter()
+  const inputRef = useRef<any>(null) //todo 
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
   })
 
   const onSubmit = useCallback(
     async (values: z.infer<typeof formSchema>) => {
+      form.reset()
       setIsLoading(true)
       if (isSignUp) {
         const data: CreateDoctorRequest = {
@@ -49,23 +61,82 @@ export default function SignUp({ formSchema, isSignUp }: Props) {
           password: values.password,
         }
         const response = await handleSignUp(data)
-        response
-          ? toastSuccess(t("SignUpSuccessMsg"))
-          : toastError(t("SomeErrorOccured"))
+        if (response) {
+          router.push("/login")
+          toastSuccess(t("SignUpSuccessMsg"))
+        } else {
+          toastError(t("SomeErrorOccured"))
+        }
       } else {
         const data: LoginDoctorRequest = {
           email: values.email,
           password: values.password,
         }
         const response = await handleLogin(data)
-        response
-          ? toastSuccess(t("LoginSuccessMsg"))
-          : toastError(t("SomeErrorOccured"))
+        if (response) {
+          router.push("/")
+          toastSuccess(t("LoginSuccessMsg"))
+        } else {
+          toastError(t("SomeErrorOccured"))
+        }
       }
       setIsLoading(false)
     },
-    [isSignUp]
+    [isSignUp, form]
   )
+
+  const onKeyDown = useCallback((e: { key: string }) => {
+    if (inputRef.current) {
+      if(e.key == ARROW_UP) {
+        const idx = currentFocussedInputIdx === 0 ? 0 : currentFocussedInputIdx - 1;
+        inputRef.current[idx].focus()
+      }
+      else if(e.key == ARROW_DOWN) {
+        const idx = currentFocussedInputIdx === inputRef.current.length - 2 ? inputRef.current.length - 2 : currentFocussedInputIdx + 1;
+        console.log({idx})
+        inputRef.current[idx].focus()
+      }
+    }
+  }, [inputRef.current, currentFocussedInputIdx])
+
+  useEffect(() => {
+    const emailInput: InputType = {
+      name: "email",
+      label: t("Email"),
+      type: "text",
+      placeholder: "Email",
+    }
+
+    const passwordInput: InputType = {
+      name: "password",
+      label: t("Password"),
+      type: "password",
+      placeholder: "Password",
+    }
+
+    const newInput = [emailInput, passwordInput]
+
+    if (isSignUp) {
+      const firstNameInput: InputType = {
+        name: "firstName",
+        label: t("FirstName"),
+        type: "text",
+        placeholder: "John",
+      }
+
+      const lastNameInput: InputType = {
+        name: "lastName",
+        label: t("LastName"),
+        type: "text",
+        placeholder: "Doe",
+      }
+
+      newInput.unshift(lastNameInput)
+      newInput.unshift(firstNameInput)
+    }
+
+    setInputs(newInput)
+  }, [isSignUp])
 
   return (
     <>
@@ -76,83 +147,34 @@ export default function SignUp({ formSchema, isSignUp }: Props) {
             {isSignUp ? t("SignUp") : t("Login")}
           </h2>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              {isSignUp && (
-                <>
-                  <FormField
-                    control={form.control}
-                    name="firstName"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>{t("FirstName")}</FormLabel>
-                        <FormControl>
-                          <Input
-                            placeholder="John"
-                            {...field}
-                            className="focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-1"
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-
-                  <FormField
-                    control={form.control}
-                    name="lastName"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>{t("LastName")}</FormLabel>
-                        <FormControl>
-                          <Input
-                            placeholder="Doe"
-                            {...field}
-                            className="focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-1"
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </>
-              )}
-
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{t("Email")}</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="you@example.com"
-                        {...field}
-                        className="focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-1"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{t("Password")}</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        placeholder="Password"
-                        {...field}
-                        className="focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-1"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-8"
+              ref={inputRef}
+              onKeyDown={onKeyDown}
+            >
+              {inputs.map((inp, index) => (
+                <FormField
+                  key={index}
+                  control={form.control}
+                  name={inp.name}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{inp.label}</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder={inp.placeholder}
+                          {...field}
+                          type={inp.type}
+                          className="focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-1"
+                          onFocus={() => setCurrentFocussedInputIdx(index)}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              ))}
               <Button
                 type="submit"
                 className="bg-blue-600 hover:bg-blue-700 text-white"
